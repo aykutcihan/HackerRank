0124771afbf2b7cd13f05a5c28f8f8aad55cf0ec
AykutCihan <aykutcihandemir@protonmail.com>
2023-05-18 12:13:59 +0200

    Java Stdin and Stdout I

--- src/main/java/org/example/JavaStdinandStdout1.java
+++ src/main/java/org/example/JavaStdinandStdout1.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import java.util.*;
+
 public class JavaStdinandStdout1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        for(int i = 0; i < 3; i++) {
+            int num = scan.nextInt();
+            System.out.println(num);
+        }
+
+        scan.close();
+
+    }
 }
